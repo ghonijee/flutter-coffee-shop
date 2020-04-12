c9560928cbc0e17c7a67bc5fdf11c90c161504a2
Ahmad Yunus Afghoni <akughoni@gmail.com>
2020-04-12 13:06:22 +0700

    finish

--- lib/view/components/menuItemcard.dart
+++ lib/view/components/menuItemcard.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_shop/model/coffeeModel.dart';
+import 'package:coffee_shop/view/pages/detaiPage.dart';
 import 'package:flutter/material.dart';
 
 class MenuItemCard extends StatelessWidget {
@@ -15,45 +16,59 @@ class MenuItemCard extends StatelessWidget {
         child: Container(
           child: Row(
             children: <Widget>[
-              AspectRatio(
-                aspectRatio: 1 / 1,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.asset(
-                    menu[index].image,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              AspectRatio(
-                aspectRatio: 4 / 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailPage(
+                                index: index,
+                              )));
+                },
+                child: Row(
                   children: <Widget>[
-                    Text(
-                      menu[index].name,
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      menu[index].shortDesc,
-                      style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey[500],
-                          fontWeight: FontWeight.w300),
+                    AspectRatio(
+                      aspectRatio: 1 / 1,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(
+                          menu[index].image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                     SizedBox(
-                      height: 20,
+                      width: 20,
                     ),
-                    Text(
-                      "Rp. " + menu[index].price.toString(),
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    )
+                    AspectRatio(
+                      aspectRatio: 4 / 3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            menu[index].name,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            menu[index].shortDesc,
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey[500],
+                                fontWeight: FontWeight.w300),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Rp. " + menu[index].price.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
---
+++ lib/view/pages/detaiPage.dart
@@ -0,0 +1,195 @@
+import 'package:coffee_shop/model/coffeeModel.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  final int index;
+
+  DetailPage({Key key, this.index}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  bool _like = false;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: width,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: height * 0.55,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(menu[widget.index].image),
+                        fit: BoxFit.cover)),
+                child: Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Colors.black.withOpacity(0.9),
+                    Colors.black.withOpacity(0.5),
+                    Colors.black.withOpacity(0.0),
+                    Colors.black.withOpacity(0.0),
+                    Colors.black.withOpacity(0.5),
+                    Colors.black.withOpacity(0.9),
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                ),
+              ),
+              Container(
+                width: width,
+                margin: EdgeInsets.only(top: height * 0.5),
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      menu[widget.index].name,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      height: 50,
+                      width: width,
+                      child: ListView.builder(
+                        itemCount: menu[widget.index].rate,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, int key) {
+                          return Icon(
+                            Icons.star,
+                            color: Colors.yellow[900],
+                            size: 34,
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Description",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      menu[widget.index].desc,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                          letterSpacing: 0.5,
+                          wordSpacing: 1.5),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Price",
+                              style: TextStyle(
+                                  color: Colors.grey[500],
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Text(
+                              menu[widget.index].price.toString(),
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        RaisedButton(
+                          onPressed: () {},
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          color: Colors.orange[800],
+                          padding: EdgeInsets.fromLTRB(35, 15, 35, 15),
+                          child: Text(
+                            "Add to cart",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 30,
+                top: height * 0.05,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.keyboard_backspace,
+                    size: 42,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 30,
+                top: height * 0.45,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _like = !_like;
+                    });
+                  },
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(35),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.5),
+                              blurRadius: 5,
+                              spreadRadius: 1)
+                        ]),
+                    child: Icon(
+                      Icons.favorite,
+                      size: 45,
+                      color: (_like) ? Colors.red : Colors.grey[600],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
